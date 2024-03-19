272fdfc5fe7f353f96b3ef8f91b11049e79c3893
Willjunger <williamjunger@hotmail.com>
2024-03-18 23:31:13 -0300

    adiciona formulario e logica para renderização condicional das noticias

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
+import { useState } from "react";
 import ArticleList from "./components/ArticleList/ArticleList";
 import Header from "./components/Header/Header";
+import Form from "./components/Form/Form";
 
 const App = () =>{
-  return <>
-  <Header/>
-  <ArticleList/>
-  </>
+  const [user, setUser] = useState()
+  const hasUser = Boolean(user)
+
+  return <div className="h-screen">
+  <Header user={user}/>
+  { hasUser && <ArticleList/>}
+  { hasUser || <Form onSubmit={setUser}/>}
+  </div>
 }
 
 export default App;
