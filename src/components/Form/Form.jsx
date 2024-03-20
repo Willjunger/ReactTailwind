262413a6de41798f27ee50c55e292c5097c16400
Willjunger <williamjunger@hotmail.com>
2024-03-19 22:16:54 -0300

    adicionar componente input e estilização

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,3 +1,5 @@
+import Input from "../Input/Input";
+
 const Form = ({onSubmit}) =>{
     const safeSubmit = event => {
         event.preventDefault();
@@ -7,10 +9,10 @@ const Form = ({onSubmit}) =>{
         const email = event.target[1].value;
         onSubmit({name, email})
     }
-    return <form onSubmit={safeSubmit} className="h-full flex flex-col items-center justify-center gap-10 -mt-20">
-        <input type="text" placeholder="Insira seu nome..."/>
-        <input type="email" placeholder="Insira seu e-mail..."/>
-        <button className="py-1 px-5 bg-alura-100 dark:bg-dark-200 rounded-full text-gray-200 uppercase" type="submit">Seguir</button>
+    return <form onSubmit={safeSubmit} className="h-full flex flex-col items-center justify-center gap-10 -mt-20 mx-5">
+        <Input required type="text" placeholder="Insira seu nome..."/>
+        <Input required type="email" placeholder="Insira seu e-mail..."/>
+        <button className="py-1 px-5 bg-alura-100 dark:bg-dark-200 rounded-full text-gray-200 uppercase outline-none w-full max-w-sm" type="submit">Seguir</button>
     </form>
 }
 
---
+++ src/components/Input/Input.jsx
@@ -0,0 +1,5 @@
+const Input  = props => {
+    return <input className="py-1 pl-5 dark:bg-dark-200 text-dark-200 dark:text-gray-200 rounded-full outline-none w-full max-w-lg" {...props}/>
+}
+
+export default Input;
