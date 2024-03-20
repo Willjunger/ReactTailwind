9db6dc7ad8c753d78e09c51685c5f5e7089df756
Willjunger <williamjunger@hotmail.com>
2024-03-19 22:22:45 -0300

    adiciona estilização do hover do botão

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -12,7 +12,7 @@ const Form = ({onSubmit}) =>{
     return <form onSubmit={safeSubmit} className="h-full flex flex-col items-center justify-center gap-10 -mt-20 mx-5">
         <Input required type="text" placeholder="Insira seu nome..."/>
         <Input required type="email" placeholder="Insira seu e-mail..."/>
-        <button className="py-1 px-5 bg-alura-100 dark:bg-dark-200 rounded-full text-gray-200 uppercase outline-none w-full max-w-sm" type="submit">Seguir</button>
+        <button className="py-1 px-5 bg-alura-100 dark:bg-dark-200 rounded-full text-gray-200 uppercase outline-none w-full max-w-sm hover:animate-pulse hover:shadow-md hover:shadow-gray-500 hover:dark:shadow-black" type="submit">Seguir</button>
     </form>
 }
 
