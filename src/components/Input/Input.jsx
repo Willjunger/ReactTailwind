4ffdb4ccb7ecae6bd76b6796a72ea2d4cc2b2e69
Willjunger <williamjunger@hotmail.com>
2024-03-19 22:34:07 -0300

    adiciona pseudo classes em todo o projeto

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,5 +1,5 @@
 const Input  = props => {
-    return <input className="py-1 pl-5 dark:bg-dark-200 text-dark-200 dark:text-gray-200 rounded-full outline-none w-full max-w-lg" {...props}/>
+    return <input className="py-1 pl-5 dark:bg-dark-200 text-dark-200 dark:text-gray-200 rounded-full outline-none w-full max-w-lg placeholder:text-gray-400 placeholder:dark:text-gray-600 placeholder:text-xs placeholder:uppercase placeholder:font-bold focus:border-2 focus:border-alura-200 focus:dark:border-alura-100 invalid:focus:border-2 invalid:focus:border-red-400 invalid:focus:dark:border-2 invalid:focus:dark:border-red-400" {...props}/>
 }
 
 export default Input;
